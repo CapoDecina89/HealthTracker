b652996eba6bcfa6c57f8d8e47b68b5ca4663ea7
capodecina89 <benjamin.grunow@iubh-fernstudium.de>
2023-01-14 14:03:12 +0100

    Initial Commit

---
+++ HealthTracker/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker
+//
+//  Created by Benjamin Grunow on 14.01.23.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
